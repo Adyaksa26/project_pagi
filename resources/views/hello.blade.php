c867a29c10f01acc8e572349e8656fba05f7b69f
Adyaksa26 <adyaksa.azky@gmail.com>
2024-06-10 09:09:39 +0700

    ini adalah project laravel msib 6

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello ini file pertama saya di dalam view laravel</h1>
+    @php
+    $nama = 'Budi';
+    $nilai = 59.00;
+    @endphp
+    {{-- struktur kendali IF --}}
+    @if ($nilai >= 60)
+    @php $ket = "lulus"; @endphp
+    @else
+    @php $ket = "tidak lulus"; @endphp
+    @endif
+
+    {{-- memanggil variabel hasil didalam laravel menggunakan kurung kurawa --}}
+    {{$nama}} <p> Dengan nilai </p> {{$nilai}} <p> Dinyatakan </p> {{$ket}}
+</body>
+</html>
