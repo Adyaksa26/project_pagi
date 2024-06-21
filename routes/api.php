b3a1d96618db166c799e27b952286673609474b5
Adyaksa26 <adyaksa.azky@gmail.com>
2024-06-21 11:33:59 +0700

    menambahkan login dan register

--- routes/api.php
+++ routes/api.php
@@ -3,13 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProdukController;
+use App\Http\Controllers\Api\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::middleware(["auth:sanctum"])->group(function() {
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
 Route::get('apiproduk', [produkController::class, 'index']);
 Route::get('apiproduk/{id}', [ProdukController::class, 'show']);
 Route::post('/produk-create', [ProdukController::class, 'store']);
 Route::put('/produk/{id}', [ProdukController::class, 'update']);
 Route::delete('/produk/{id}', [ProdukController::class, 'destroy']);
+});
